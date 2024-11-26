76b1f0d8640e8d2a4eee6ee35f36699f444a5b92
Alexis Dupire <adupire4@gmail.com>
2024-11-26 17:50:12 +0100

    [add] Navigation buttons

---
+++ MoidaMansion/Assets/Scripts/PlayerControlelr.cs
@@ -0,0 +1,50 @@
+using System;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem.iOS;
+using UnityEngine.UI;
+using Vector3 = System.Numerics.Vector3;
+
+public class PlayerController : MonoBehaviour
+
+{
+    [SerializeField]
+    Button rightButton;
+    Vector2 _position = new Vector2(0, 0);
+    
+    public void SwitchLevel(int doorDirection)
+    {
+        _position += new Vector2(0, doorDirection);
+        Debug.Log("New position = " + _position);
+    }
+    
+    public void SwitchRoom(int direction)
+    {
+        if (direction > 0)
+        {
+            if (_position.x < 3)
+            {
+                _position += new Vector2(direction, 0);
+            }else return;
+        }else
+        if(_position.x > 0)
+            _position += new Vector2(direction, 0);
+        
+        Debug.Log("hello guys");
+    }
+
+    // void inspectItem(itemToInspect)
+    // {
+    //     if (itemToInspect.isButton) ;
+    //         if(inventory.friends.count>0)
+    //             // Take a friend to push the button
+    //     if (itemToInspect.isFalse) ;
+    //         // Dialogue nothing here.
+    //         // wrongSearch ++ ( for the Monsta spawn systeme)
+    //     if (itemToInspect.isFriend) ;
+    //         // Display friend found + dialogue
+    //         // Add friend to the inventory
+    // }
+}
